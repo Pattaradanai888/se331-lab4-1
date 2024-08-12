bf273ae809550f3b63cc3abf240b08a7e2951bda
Pattaradanai888 <pattaradanai_t@cmu.ac.th>
2024-08-12 15:57:39 +0700

    change css class to used tailwind instead

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const updatePageSize = (size: number) => {
 </script>
 
 <template>
-  <div id="layout">
+  <div class="text-center font-sans text-gray-700 antialiased">
     <header>
       <div id="flashMessage" v-if="message">
         <h4>{{ message }}</h4>
@@ -45,14 +45,6 @@ const updatePageSize = (size: number) => {
 </template>
 
 <style>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
   padding: 30px;
 }
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -42,7 +42,7 @@ onMounted(() => {
 <template>
   <h1>Events For Good</h1>
   <!-- new element -->
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <div class="pagination">
       <RouterLink
@@ -64,12 +64,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
 .pagination {
   display: flex;
   width: 290px;
