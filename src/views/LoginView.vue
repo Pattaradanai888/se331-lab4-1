485ffecf88ffadf8d6de42c43f4ba1c4cc66c159
Pattaradanai888 <pattaradanai_t@cmu.ac.th>
2024-10-10 21:20:10 +0700

    LAB12 - 4.5

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,37 +1,45 @@
 <script setup lang="ts">
-    import InputText from '@/components/InputText.vue';
-    import { ref } from 'vue';
-    import * as yup from 'yup'
-    import { useField, useForm } from 'vee-validate';
-    const validationSchema = yup.object({
-        email: yup.string().required('The email is required').email('Input must be and email.'),
-        password: yup.string().required('The password is required').min(6,'The password must be at least 6 character.')
+import InputText from '@/components/InputText.vue';
+import { ref } from 'vue';
+import * as yup from 'yup'
+import { useField, useForm } from 'vee-validate';
+import { useAuthStore } from '@/stores/auth';
+const authStore = useAuthStore();
+const validationSchema = yup.object({
+    email: yup.string().required('The email is required'),
+    password: yup.string().required('The password is required')
+})
+const { errors, handleSubmit } = useForm({
+    validationSchema,
+    initialValues: {
+        email: '',
+        password: ''
+    }
+})
+const { value: email } = useField<string>('email')
+const { value: password } = useField<string>('password')
+const onSubmit = handleSubmit((values) => {
+    authStore.login(values.email, values.password)
+    .then(() => {
+        console.log('login success')
+    }).catch((err) => {
+        console.log('error',err)
     })
-    const { errors , handleSubmit} = useForm({
-        validationSchema,
-        initialValues: {
-            email: '',
-            password: ''
-        }
-    })
-    const { value: email } = useField<string>('email')
-    const { value: password } = useField<string>('password')
-    const onSubmit = handleSubmit((values) => {
-        console.log(values)
-    })
+})
 </script>
 <template>
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="mx-auto h-10 w-auto"
-             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company">
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
+                account</h2>
         </div>
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" @submit.prevent="onSubmit" method="POST">
                 <div>
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                    <InputText type="email" v-model="email" placeholder="Email address" :error="errors['email']"/>
+                    <InputText type="text" v-model="email" placeholder="Email address" :error="errors['email']" />
                 </div>
                 <div>
                     <div class="flex items-center justify-between">
@@ -40,16 +48,18 @@
                             <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                         </div>
                     </div>
-                    <InputText type="password" v-model="password" placeholder="Password" :error="errors['password']"/>
+                    <InputText type="password" v-model="password" placeholder="Password" :error="errors['password']" />
                 </div>
                 <div>
-                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         Sign in
                     </button>
                 </div>
             </form>
             <p class="mt-10 text-center text-sm text-gray-500">Not a member? {{ '' }}
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Try to register here</a>
+                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Try to register
+                    here</a>
             </p>
         </div>
     </div>
